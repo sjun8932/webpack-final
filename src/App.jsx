ff3b3add2c684c9b111dd531526a8ee716180f1f
SANGJUN <sjun8932@gmail.com>
2022-06-18 10:28:05 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const App = () => {
+    return(
+        <>
+            <div className="a">안뇽</div>
+        </>
+    )
+};
+
+export default App;
---
+++ src/client.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style.css';
+
+import TestApp from './App';
+
+
+
+ReactDom.render(<TestApp />, document.querySelector('#root'));
